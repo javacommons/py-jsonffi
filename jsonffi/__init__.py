b72f4db591787b0a73ac31685d9fd433869d6b9c
JavaCommons Technologies <javacommons@gmail.com>
2024-04-28 13:23:12 +0900

    .

--- jsonffi/__init__.py
+++ jsonffi/__init__.py
@@ -2,7 +2,7 @@ import json
 from cffi import FFI
 
 __copyright__    = 'Copyright (C) 2024 JavaCommons Technologies'
-__version__      = '1.0.4'
+__version__      = '1.0.6'
 __license__      = 'MIT'
 __author__       = 'JavaCommons Technologies'
 __author_email__ = 'javacommmons@gmail.com'
@@ -13,13 +13,22 @@ class JsonFFI:
     def __init__(self, dll):
         self.ffi = FFI()
         self.ffi.cdef("const char *Call(const char *, const char *);")
-        self.ffi.cdef("const char *LastError();")
         self.clib = self.ffi.dlopen(dll)
     def call(self, name, args):
         answer = self.ffi.string(self.clib.Call(name.encode(), json.dumps(args).encode())).decode()
-        error = self.ffi.string(self.clib.LastError()).decode()
-        if error == "": return json.loads(answer)
-        raise Exception(error)
+        answer = str.strip(answer)
+        if answer.startswith('"'):
+            error = json.loads(answer)
+            raise Exception(error)
+        elif answer.startswith('['):
+            list = json.loads(answer)
+            if len(list) == 0:
+                return None
+            else:
+                return list[0]
+        else:
+            error = f'Malformed result json: {answer}'
+            raise Exception(error)
 
 class MyJS:
     def __init__(self, myjsDll, asmSpecList = []):
